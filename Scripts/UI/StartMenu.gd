1767eff98a789f305958ed47c3596560c209ca2a
Pontus <pontus.goth@outlook.com>
2023-02-05 13:53:56 +0100

    committing

--- Scripts/UI/StartMenu.gd
+++ Scripts/UI/StartMenu.gd
@@ -1,13 +1,14 @@
 extends Control
 
 onready var startbtn = $MenuItems/StartButton
-
-
+onready var audio = $AudioStreamPlayer
+onready var rain= $rain
 
 
 func _ready():
 	startbtn.grab_focus()
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	rain.play()
 	
 	
 func _on_StartButton_pressed():
@@ -16,15 +17,6 @@ func _on_StartButton_pressed():
 
 func _on_ExitButton_pressed():
 	get_tree().quit()
-
-
-#func _on_Start_focus_exited():
-#	Buttonsound.play()
-
-
-
-#func _on_Exit_focus_exited():
-	#Buttonsound.play()
 
 
 
