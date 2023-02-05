7e2a63b604340b65b07c3b9e2963880bfc4bd634
DavidBystroem <47114105+DavidBystroem@users.noreply.github.com>
2023-02-05 13:51:38 +0100

    bedroom walls

--- Scripts/UI/Endgame.gd
+++ Scripts/UI/Endgame.gd
@@ -7,9 +7,9 @@ onready var moodscore = $MoodScoreResult
 func _ready():
 	startbtn.grab_focus()
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	if GlobalVariables.MoodScore >=40:
+	if GlobalVariables.MoodScore as int >=40:
 		moodscore.text = "You've started making the small steps towards a better life. Keep it up!"
-	if GlobalVariables.MoodScore <=30:
+	if GlobalVariables.MoodScore as int <=30:
 		moodscore.text = "You're still stuck in your old ways and your 'roots' are still growing around you..."
 		
 func _on_Return_pressed():
