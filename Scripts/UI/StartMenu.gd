2f55984d5b1c37f1206ed263b8e4f24e66a9d61d
DavidBystroem <47114105+DavidBystroem@users.noreply.github.com>
2023-02-05 12:38:29 +0100

    Things

--- Scripts/UI/StartMenu.gd
+++ Scripts/UI/StartMenu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-onready var startbtn = $MenuItems/Start
+onready var startbtn = $MenuItems/StartButton
 onready var Buttonsound = $Buttonsound
 
 
@@ -9,23 +9,23 @@ func _ready():
 	startbtn.grab_focus()
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
-	MusicController.playgame()
 	
-func _on_Start_pressed():
-	get_tree().change_scene("res://GameScene/GameWorld.tscn")
+func _on_StartButton_pressed():
+	get_tree().change_scene("res://levels/FirstScene.tscn")
 
 
-
-func _on_Exit_pressed():
+func _on_ExitButton_pressed():
 	get_tree().quit()
 
 
-
-func _on_Start_focus_exited():
-	Buttonsound.play()
-
+#func _on_Start_focus_exited():
+#	Buttonsound.play()
 
 
-func _on_Exit_focus_exited():
-	Buttonsound.play()
+
+#func _on_Exit_focus_exited():
+	#Buttonsound.play()
+
+
+
 
